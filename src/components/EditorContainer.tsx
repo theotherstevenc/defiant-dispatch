5aa710f2ea387f8c274e34e34f65feb4613572f6
theotherstevenc <steven.winston.collins@gmail.com>
2026-01-29 14:44:54 -0800

    refactor: enhance split sizes and collapse state managment

--- src/components/EditorContainer.tsx
+++ src/components/EditorContainer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from '@mui/material'
 import { clsx } from 'clsx'
 import { useEffect } from 'react'
@@ -9,39 +8,42 @@ import {
   EDITOR_CONTAINER_SPLIT_SIZES_DEFAULT,
   EDITOR_CONTAINER_SPLIT_SIZES_MINIMUM,
   EDITOR_CONTAINER_SPLIT_SIZES_STORAGE_KEY,
+  EDITOR_SPLIT_COLLAPSE_THRESHOLD,
+  EDITOR_SPLIT_COLLAPSED_STATE,
 } from '../utils/constants'
 import usePersistentValue from '../utils/usePersistentValue'
 
 import EditorWorkingFiles from './EditorWorkingFiles'
 import EditorWorkspacePreview from './EditorWorkspacePreview'
 
+type SplitSizes = [number, number]
+
 const EditorContainer = () => {
-  const { hideWorkingFiles, setHideWorkingFiles } = useAppContext()
+  const { setHideWorkingFiles } = useAppContext()
 
-  const [sizes, setSizes] = usePersistentValue(EDITOR_CONTAINER_SPLIT_SIZES_STORAGE_KEY, EDITOR_CONTAINER_SPLIT_SIZES_DEFAULT)
-  const minThreshold = 5
+  const [sizes, setSizes] = usePersistentValue<SplitSizes>(EDITOR_CONTAINER_SPLIT_SIZES_STORAGE_KEY, EDITOR_CONTAINER_SPLIT_SIZES_DEFAULT)
 
-  const handleDrag = (newSizes: number[]) => {
-    setHideWorkingFiles(false)
+  // Derive collapsed state from sizes (single source of truth)
+  const isCollapsed = sizes[0] < EDITOR_SPLIT_COLLAPSE_THRESHOLD
+
+  // Sync to AppContext whenever collapsed state changes
+  useEffect(() => {
+    setHideWorkingFiles(isCollapsed)
+  }, [isCollapsed, setHideWorkingFiles])
+
+  const handleDrag = (newSizes: SplitSizes) => {
     setSizes(newSizes)
   }
 
-  const handleDragEnd = (newSizes: number[]) => {
-    const isWorkingFilesHidden = newSizes[0] < minThreshold
-    setHideWorkingFiles(isWorkingFilesHidden)
-    setSizes(isWorkingFilesHidden ? [0, 100] : newSizes)
+  const handleDragEnd = (newSizes: SplitSizes) => {
+    const willCollapse = newSizes[0] < EDITOR_SPLIT_COLLAPSE_THRESHOLD
+    setSizes(willCollapse ? EDITOR_SPLIT_COLLAPSED_STATE : newSizes)
   }
 
   const workingFilesClassName = clsx({
-    'no-working-files': hideWorkingFiles,
-    'warn-collapse': sizes[0] < minThreshold,
+    'no-working-files': isCollapsed,
+    'warn-collapse': sizes[0] < EDITOR_SPLIT_COLLAPSE_THRESHOLD && !isCollapsed,
   })
-
-  useEffect(() => {
-    if (!hideWorkingFiles && sizes[0] < minThreshold) {
-      setSizes(EDITOR_CONTAINER_SPLIT_SIZES_DEFAULT)
-    }
-  }, [hideWorkingFiles])
 
   return (
     <Split className='split-component' sizes={sizes} minSize={EDITOR_CONTAINER_SPLIT_SIZES_MINIMUM} onDragEnd={handleDragEnd} onDrag={handleDrag}>
@@ -54,4 +56,5 @@ const EditorContainer = () => {
     </Split>
   )
 }
+
 export default EditorContainer
