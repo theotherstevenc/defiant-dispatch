3ece56b084c1766719aa1a8d29f26e3ce8c338d1
theotherstevenc <steven.winston.collins@gmail.com>
2026-01-29 14:04:54 -0800

    refactor: updating imports to use each component file instead of the barrel

--- src/components/EditorConfigActions.tsx
+++ src/components/EditorConfigActions.tsx
@@ -1,22 +1,21 @@
 import { Box } from '@mui/material'
 
-import {
-  Authenticator,
-  FontSizeControls,
-  InputCreateNewFile,
-  InputDeleteFile,
-  InputFileUpload,
-  InputFormatHTML,
-  InputLockFile,
-  InputMarkupSettings,
-  InputSenderSettings,
-  InputThemeToggle,
-  InputToggleEditorTheme,
-  InputTogglePreviewTheme,
-  InputToggleWorkingFiles,
-  InputUpdateFiles,
-} from '../components'
 import { inputActionsStyles, inputConfigStyles, inputSenderSettingsStyles } from '../styles/global.styles'
+
+import Authenticator from './Authenticator'
+import FontSizeControls from './FontSizeControls'
+import InputCreateNewFile from './InputCreateNewFile'
+import InputDeleteFile from './InputDeleteFile'
+import InputFileUpload from './InputFileUpload'
+import InputFormatHTML from './InputFormatHTML'
+import InputLockFile from './InputLockFile'
+import InputMarkupSettings from './InputMarkupSettings'
+import InputSenderSettings from './InputSenderSettings'
+import InputThemeToggle from './InputThemeToggle'
+import InputToggleEditorTheme from './InputToggleEditorTheme'
+import InputTogglePreviewTheme from './InputTogglePreviewTheme'
+import InputToggleWorkingFiles from './InputToggleWorkingFiles'
+import InputUpdateFiles from './InputUpdateFiles'
 
 const EditorConfigActions = () => (
   <Box sx={inputConfigStyles}>
